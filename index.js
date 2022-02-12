8f6ff376bfb52a678909cc130cf66de10c343fe8
zouhair <ettarak.zouhair@gmail.com>
2022-02-12 21:30:51 +0100

    Prepare Router auth

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
+// import 
+const authRouter = require('./router/auth');
+
+app.use('/api/user', authRouter);
 
 app.listen(3000, () => console.log('Server up running.....!'));
--- router/auth.js
+++ router/auth.js
@@ -1,4 +1,12 @@
-const router = require5('express').router;
+const router = require('express').Router();
+
+router.post('/register', (req, res) =>  {
+    res.send('Register path');
+});
+
+router.post('/login' , (req, res) =>  {
+    res.send('login');
+});
 
 
-exports.module = router;
+module.exports = router;
