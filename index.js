7110f3e968a354771fdab9e1955a4d6f5594ccb8
zouhair <ettarak.zouhair@gmail.com>
2022-02-12 21:37:58 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-// import 
+// import router auth ( login and register )
 const authRouter = require('./router/auth');
 
+// import
 app.use('/api/user', authRouter);
 
 app.listen(3000, () => console.log('Server up running.....!'));
