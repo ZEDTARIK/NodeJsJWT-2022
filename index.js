aa37f4f99a44a3622a2521098c2dbf493b3c426a
zouhair <ettarak.zouhair@gmail.com>
2022-02-19 18:07:16 +0100

    add configuration database

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Connect to database
+mongoose.connect(process.env.DB_CONNECT, ()=> console.log('connect'))
 // import router auth ( login and register )
 const authRouter = require('./router/auth');
 
